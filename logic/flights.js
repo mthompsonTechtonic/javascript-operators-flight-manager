cb5566ec9c96987af0815feeacbab93bacfbf26b
mthompsonTechtonic <mark.thompson@techtonic.com>
2020-10-29 16:11:34 -0500

    1st commit

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,13 @@
 function Flights() {
 
+    function calculateNumberOfFlights(pass,cap,){
+        let f = cap/pass
+        if(cap % pass){
+            return f
+        }else{
+            return Math.trunc(f+1)
+        }
+    }
+
 }
 
